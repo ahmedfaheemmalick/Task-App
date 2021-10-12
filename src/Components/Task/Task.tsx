11ec5a3b388e7fdbe39e7abf031bab0dcd7cf01f
AhmedMalick <ahmedmalik2k20@gmail.com>
2021-10-12 14:16:44 +0500

    Update Task.tsx

--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -1,23 +1,28 @@
 import { FC } from "react";
+import { AnyAction } from "redux";
 import TaskType from "../../Interfaces/TaskType.interface";
 import { store } from "../../Store/index";
 import styles from "./Task.module.css";
 
 interface Props {
   task: TaskType;
-  archiveTask: any;
-  pinTask: any;
+  archiveTask: (id: number) => AnyAction;
+  pinTask: (id: number) => AnyAction;
 }
 
-const Task: FC<Props> = ({ task, archiveTask, pinTask }) => {
+const Task: FC<Props> = ({
+  task: { task, state, id },
+  archiveTask,
+  pinTask,
+}) => {
   return (
     <div className={styles.task}>
-      <p>{task.task}</p>
-      <p>{task.state}</p>
-      <button onClick={() => store.dispatch(archiveTask(task.id))}>
-        Archive task
+      <p>{task}</p>
+      <p>{state}</p>
+      <button onClick={() => store.dispatch(archiveTask(id))}>
+        Archive Task
       </button>
-      <button onClick={() => store.dispatch(pinTask(task.id))}>Pin task</button>
+      <button onClick={() => store.dispatch(pinTask(id))}>Pin Task</button>
     </div>
   );
 };
